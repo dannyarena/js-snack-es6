3386ab06edd0c16a64d60b4d3ac9bd0e9fbe0ca4
dannyarena <arenadanny71@gmail.com>
2025-04-14 15:52:59 +0200

    Add initial biciclette array with bike names and weights

---
+++ script.js
@@ -0,0 +1,19 @@
+const biciclette = [
+    {
+      nome: "Bianchi",
+      peso: 7.5
+    },
+    {
+      nome: "Scott",
+      peso: 6.8
+    },
+    {
+      nome: "Cannondale",
+      peso: 7.2
+    },
+    {
+      nome: "Trek",
+      peso: 6.9
+    }
+  ];
+  
