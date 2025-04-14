7fe0a35ef16e2f83504a9a5ff09aeadb56349086
dannyarena <arenadanny71@gmail.com>
2025-04-14 16:11:13 +0200

    improved general code readability and fix cicle

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 const biciclette = [
-    {
-      nome: "Bianchi",
-      peso: 7.5
-    },
-    {
-      nome: "Scott",
-      peso: 6.8
-    },
-    {
-      nome: "Cannondale",
-      peso: 7.2
-    },
-    {
-      nome: "Trek",
-      peso: 6.9
-    }
+    { nome: "Bianchi", peso: 7.5 },
+    { nome: "Scott", peso: 6.8 },
+    { nome: "Cannondale", peso: 7.2 },
+    { nome: "Trek", peso: 6.9 }
   ];
   
   let biciPiuLeggera = biciclette[0];
-
+  
   for (let i = 1; i < biciclette.length; i++) {
     const bici = biciclette[i];
     
+    if (bici.peso < biciPiuLeggera.peso) {
+      biciPiuLeggera = bici;
+    }
   }
   
-  if (bici.peso < biciPiuLeggera.peso) {
-    biciPiuLeggera = bici;
-  }
-
   console.log("La bici più leggera è:");
   console.log(`Nome: ${biciPiuLeggera.nome}, Peso: ${biciPiuLeggera.peso} kg`);
+  
